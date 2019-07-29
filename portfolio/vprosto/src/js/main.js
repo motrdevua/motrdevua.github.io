5acdbf2cd9da9702a446665d697434b766f48474
Igor Lukashev <motrdevua@gmail.com>
2019-07-29 19:02:04 +0300

    started vprosto

---
+++ portfolio/vprosto/src/js/main.js
@@ -0,0 +1,6 @@
+import $ from 'jquery';
+import 'fullpage.js/dist/jquery.fullpage.min';
+import 'fullpage.js/dist/jquery.fullpage.extensions.min';
+import './fullPage-settings';
+import './burger';
+import './modal';
