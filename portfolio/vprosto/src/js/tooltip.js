3eef3ef7fd8f3ed8258c71b9cc7c78c3475813ab
Igor Lukashev <motrdevua@gmail.com>
2019-08-02 17:21:39 +0300

    a few fixes

--- portfolio/vprosto/src/js/tooltip.js
+++ portfolio/vprosto/src/js/tooltip.js
@@ -2,8 +2,11 @@ $(document).ready(function() {
   $('.tooltip').tooltipster({
     contentCloning: true,
     animation: 'grow',
-    delay: 100,
+    delay: [100, 100],
+    animationDuration: [100, 100],
+    maxWidth: 220,
     theme: ['tooltipster-noir', 'tooltipster-noir-customized'],
     arrow: false,
+    plugins: ['follower'],
   });
 });
