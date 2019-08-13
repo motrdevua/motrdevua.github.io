0a640860015c9e5e07ca5148e54a8404d8bb25f8
Igor Lukashev <motrdevua@gmail.com>
2019-08-13 20:39:25 +0300

    tourism in process

--- portfolio/tourism/src/js/slider.js
+++ portfolio/tourism/src/js/slider.js
@@ -2,8 +2,8 @@ import Swiper from 'swiper';
 
 $(document).ready(function() {
   // eslint-disable-next-line no-unused-vars
-  const mySwiper = new Swiper('.swiper-container', {
-    // autoplay: true,
+  const banner = new Swiper('.swiper-banner', {
+    autoplay: true,
     delay: 3000,
     speed: 700,
     spaceBetween: 0,
@@ -12,6 +12,19 @@ $(document).ready(function() {
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
+    },
+  });
+  // eslint-disable-next-line no-unused-vars
+  const offer = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    centeredSlides: true,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    navigation: {
+      prevEl: '.swiper-button-prev.swiper-button-prev-offer',
+      nextEl: '.swiper-button-next.swiper-button-next-offer',
     },
   });
 });
