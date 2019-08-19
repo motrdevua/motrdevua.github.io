71b6affe047d605c06879e2c250a9fdf28b0b94d
Igor Lukashev <motrdevua@gmail.com>
2019-08-19 16:05:10 +0300

    a few visual fixes

--- profile/src/js/right.js
+++ profile/src/js/right.js
@@ -1,9 +1,31 @@
 $(function() {
-  const panel = $('.panel');
-  panel.on('click', function() {
+  const title = $('.panel__title');
+  const panelContainer = $('.panel__container');
+  title.on('click', function() {
     $(this)
+      .parent()
       .toggleClass('panel--active')
       .siblings()
       .removeClass('panel--active');
+    $(this)
+      .addClass('panel__title--active')
+      .parent()
+      .siblings()
+      .children()
+      .removeClass('panel__title--active');
+  });
+  panelContainer.on('click', function() {
+    if (
+      $(this)
+        .parent()
+        .hasClass('panel--active')
+    ) {
+      $(this)
+        .parent()
+        .removeClass('panel--active');
+      $(this)
+        .prev()
+        .removeClass('panel__title--active');
+    }
   });
 });
--- profile/src/js/theme.js
+++ profile/src/js/theme.js
@@ -22,3 +22,27 @@ toggleTheme.on('click', function() {
     document.documentElement.setAttribute('data-theme', 'dark');
   }
 });
+
+$('.toggle-box .icon').on('click', function() {
+  if ($(this).hasClass('icon--lightbulb-off')) {
+    $(this).fadeOut(100);
+    $(this)
+      .removeClass('icon--lightbulb-off')
+      .addClass('icon--lightbulb-on');
+    $('.icon--lightbulb-on use').attr(
+      'xlink:href',
+      'img/spriteSvg.svg#sprite-lightbulb-on'
+    );
+    $(this).fadeIn(300);
+  } else {
+    $(this).fadeOut(100);
+    $(this)
+      .removeClass('icon--lightbulb-on')
+      .addClass('icon--lightbulb-off');
+    $('.icon--lightbulb-off use').attr(
+      'xlink:href',
+      'img/spriteSvg.svg#sprite-lightbulb-off'
+    );
+    $(this).fadeIn(300);
+  }
+});
