e3cd429af086ff3f8cf6e7268798f9c99016781a
Igor Lukashev <motrdevua@gmail.com>
2019-08-28 21:52:07 +0300

    version 2.0

--- profile/src/js/parallax.js
+++ profile/src/js/parallax.js
@@ -16,6 +16,14 @@ function transformPanel(mouseEvent) {
   const percentX = (mouseX - centerX) / (myPanel.clientWidth / 2);
   const percentY = -((mouseY - centerY) / (myPanel.clientHeight / 2));
 
+  subPanel.style.msTransform = `perspective(400px) rotateY(${percentX *
+    transformAmount}deg) rotateX(${percentY * transformAmount}deg)`;
+  subPanel.style.webkitTransform = `perspective(400px) rotateY(${percentX *
+    transformAmount}deg) rotateX(${percentY * transformAmount}deg)`;
+  subPanel.style.MozTransform = `perspective(400px) rotateY(${percentX *
+    transformAmount}deg) rotateX(${percentY * transformAmount}deg)`;
+  subPanel.style.OTransform = `perspective(400px) rotateY(${percentX *
+    transformAmount}deg) rotateX(${percentY * transformAmount}deg)`;
   subPanel.style.transform = `perspective(400px) rotateY(${percentX *
     transformAmount}deg) rotateX(${percentY * transformAmount}deg)`;
 }
@@ -24,15 +32,33 @@ function handleMouseEnter() {
   setTimeout(() => {
     subPanel.style.transition = '';
   }, 100);
+  subPanel.style.msTransition = 'transform 0.1s';
+  subPanel.style.webkitTransition = 'transform 0.1s';
+  subPanel.style.MozTransition = 'transform 0.1s';
+  subPanel.style.OTransition = 'transform 0.1s';
   subPanel.style.transition = 'transform 0.1s';
 }
 
 function handleMouseLeave() {
+  subPanel.style.msTransition = 'transform 0.1s';
+  subPanel.style.webkitTransition = 'transform 0.1s';
+  subPanel.style.MozTransition = 'transform 0.1s';
+  subPanel.style.OTransition = 'transform 0.1s';
   subPanel.style.transition = 'transform 0.1s';
   setTimeout(() => {
+    subPanel.style.msTransition = '';
+    subPanel.style.webkitTransition = '';
+    subPanel.style.MozTransition = '';
+    subPanel.style.OTransition = '';
     subPanel.style.transition = '';
   }, 100);
 
+  subPanel.style.msTransform = 'perspective(400px) rotateY(0deg) rotateX(0deg)';
+  subPanel.style.webkitTransform =
+    'perspective(400px) rotateY(0deg) rotateX(0deg)';
+  subPanel.style.MozTransform =
+    'perspective(400px) rotateY(0deg) rotateX(0deg)';
+  subPanel.style.OTransform = 'perspective(400px) rotateY(0deg) rotateX(0deg)';
   subPanel.style.transform = 'perspective(400px) rotateY(0deg) rotateX(0deg)';
 }
 
--- profile/src/js/right.js
+++ profile/src/js/right.js
@@ -1,31 +1,31 @@
 $(function() {
-  const title = $('.panel__title');
-  const panelContainer = $('.panel__container');
+  const title = $('.section__title');
+  const sectionContainer = $('.section__container');
   title.on('click', function() {
     $(this)
       .parent()
-      .toggleClass('panel--active')
+      .toggleClass('section--active')
       .siblings()
-      .removeClass('panel--active');
+      .removeClass('section--active');
     $(this)
-      .addClass('panel__title--active')
+      .addClass('section__title--active')
       .parent()
       .siblings()
       .children()
-      .removeClass('panel__title--active');
+      .removeClass('section__title--active');
   });
-  panelContainer.on('click', function() {
+  sectionContainer.on('click', function() {
     if (
       $(this)
         .parent()
-        .hasClass('panel--active')
+        .hasClass('section--active')
     ) {
       $(this)
         .parent()
-        .removeClass('panel--active');
+        .removeClass('section--active');
       $(this)
         .prev()
-        .removeClass('panel__title--active');
+        .removeClass('section__title--active');
     }
   });
 });
